c7e3e838a64b8ea539ac06b68d0a0c6f12a72388
codenameManuel <manuelfranky0@gmail.com>
2024-11-29 08:39:19 +0100

    fixed the route for cps

--- src/routes/cps.route.js
+++ src/routes/cps.route.js
@@ -4,7 +4,7 @@ import cps_controller from  "../controllers/cps.controller.js"
 const route = Router();
 
 route.get("/", cps_controller.getAllReport)
-route.get(":id", cps_controller.getOneReport)
+route.get("/:id", cps_controller.getOneReport)
 route.post("/", cps_controller.createReport)
 
 export default route;
